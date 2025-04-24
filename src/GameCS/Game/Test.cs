c7619f7162c6d55eabd6260c2d5b99d26719136b
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-24 23:28:36 +1000

    Create C# test method.

---
+++ src/GameCS/Game/Test.cs
@@ -0,0 +1,10 @@
+﻿namespace Game
+{
+    public static class Test
+    {
+        public static void TestMethod()
+        {
+            Console.WriteLine("Test Func called from C#!");
+        }
+    }
+}
