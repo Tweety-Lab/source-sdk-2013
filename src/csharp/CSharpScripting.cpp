0d6796c43238702ba10dfb5d99b64993b2047071
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-24 23:10:28 +1000

    Add CSharp placeholder methods.

--- src/csharp/CSharpScripting.cpp
+++ src/csharp/CSharpScripting.cpp
@@ -1,2 +1,27 @@
 #include "cbase.h"
 #include "CSharpScripting.h"
+
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+#include <mono/metadata/debug-helpers.h>
+
+
+// Init Mono runtime
+void CSharpScripting::InitMono()
+{
+    // TODO: Add your implementation code here.
+}
+
+
+// Cleanup Mono runtime
+void CSharpScripting::CleanupMono()
+{
+    // TODO: Add your implementation code here.
+}
+
+// Run a C# method
+// Example: RunCSharpMethod(NameSpace.Class::Method)
+void CSharpScripting::RunCSharpMethod(std::string method)
+{
+    // TODO: Add your implementation code here.
+}
--- src/csharp/CSharpScripting.h
+++ src/csharp/CSharpScripting.h
@@ -1,5 +1,10 @@
 #pragma once
+#include <string>
 static class CSharpScripting
 {
+public:
+	static void InitMono();
+	static void CleanupMono();
+	static void RunCSharpMethod(std::string method);
 };
 
